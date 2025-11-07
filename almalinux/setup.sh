297e7b4b423f25c4e609ca94df40a4b48549dc54
Wayne.MJ <150761502+wayne-mj@users.noreply.github.com>
2025-11-07 13:38:17 +1000

    Fixed script for Almalinux

--- almalinux/setup.sh
+++ almalinux/setup.sh
@@ -10,11 +10,11 @@ fi
 sudo dnf install epel-release -y
 
 ### Adding Brave Browser repository
-sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
-sudo dnf install brave-browser
+# sudo dnf config-manager addrepo --from-repofile=https://brave-browser-rpm-release.s3.brave.com/brave-browser.repo
+# sudo dnf install brave-browser
 
 ### Adding support for VS Code
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
-dnf check-update
-sudo dnf install code # or code-insiders
+# sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+# echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+# dnf check-update
+# sudo dnf install code # or code-insiders
