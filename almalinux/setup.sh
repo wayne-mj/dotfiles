557e2cdf64ca42c92a46fb65b93e8830f98c83ff
Wayne.MJ <150761502+wayne-mj@users.noreply.github.com>
2025-11-10 09:30:22 +1000

    Added NVM and CRB

--- almalinux/setup.sh
+++ almalinux/setup.sh
@@ -7,9 +7,24 @@ if [ -x /usr/bin/flatpak ]; then
   https://dl.flathub.org/repo/flathub.flatpakrepo
 fi
 
+### Enable CRB
+echo "Enabling CRB repository"
+sudo dnf config-manager --enable crb
+
 ### Add Extra packages for Enterprise linux
 echo "Adding extra packages"
 sudo dnf install epel-release -y
+
+### Install Node Version Manager (nvm) from its repository
+echo "Install Node Version Manager"
+owner="nvm-sh"
+repo="nvm"
+# Get the latest release tag using the GitHub API
+latest_release_tag=$(curl -s "https://api.github.com/repos/$owner/$repo/releases/latest" | grep -oP '"tag_name": "\K[^"]+')
+if [ ! -z "$latest_release_tag" ]; then
+  install_sh_url="https://raw.githubusercontent.com/$owner/$repo/$latest_release_tag/install.sh"
+  curl -o- "$install_sh_url" | bash
+fi
 
 ### Adding Brave Browser repository
 echo "Installing Brave Browser"
@@ -39,3 +54,4 @@ sudo dnf install code -y # or code-insiders
 
 # ### Installing Conda
 # sudo dnf install conda -y
+
