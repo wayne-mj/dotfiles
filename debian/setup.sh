b1f48ef4982c959e088eb3b054dd0663ac36ad6e
Wayne.MJ <150761502+wayne-mj@users.noreply.github.com>
2025-11-15 21:27:59 +1000

    Updated Debian scripts

--- debian/setup.sh
+++ debian/setup.sh
@@ -9,16 +9,25 @@ PKG="wget curl gpg apt-transport-https git ca-certificates htop build-essential 
 echo "Adding the following packages:"
 echo $PKG
 
+sudo apt update
 # Install required packages and dependencies   
 sudo apt install -y $PKG && touch .success
 
 ### Install Node Version Manager (nvm) from its repository
-# echo "Install Node Version Manager"
-# owner="nvm-sh"
-# repo="nvm"
-# # Get the latest release tag using the GitHub API
-# latest_release_tag=$(curl -s "https://api.github.com/repos/$owner/$repo/releases/latest" | grep -oP '"tag_name": "\K[^"]+')
-# if [ ! -z "$latest_release_tag" ]; then
-#   install_sh_url="https://raw.githubusercontent.com/$owner/$repo/$latest_release_tag/install.sh"
-#   curl -o- "$install_sh_url" | bash
-# fi
+echo "Install Node Version Manager"
+owner="nvm-sh"
+repo="nvm"
+# Get the latest release tag using the GitHub API
+latest_release_tag=$(curl -s "https://api.github.com/repos/$owner/$repo/releases/latest" | grep -oP '"tag_name": "\K[^"]+')
+if [ ! -z "$latest_release_tag" ]; then
+  install_sh_url="https://raw.githubusercontent.com/$owner/$repo/$latest_release_tag/install.sh"
+  curl -o- "$install_sh_url" | bash
+fi
+
+
+### Installing Brave Browser
+echo "Installing Brave Browser"
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+sudo curl -fsSLo /etc/apt/sources.list.d/brave-browser-release.sources https://brave-browser-apt-release.s3.brave.com/brave-browser.sources
+sudo apt update
+sudo apt install brave-browser
